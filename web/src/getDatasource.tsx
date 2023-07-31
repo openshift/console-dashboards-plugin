fa38a2a8348717e81355838a421fdc536d64c520
Jenny Zhu <jenny.a.zhu@gmail.com>
2023-07-31 16:53:59 -0400

    modify getDatasources.tsx to no longer be an async function because the monitoring-plugin now uses useResolvedExtensions() instead of useExtensions(). useResolvedExtensions is a hook that will re-render  the Dashboards page once the extensions is extensions is resolved.

--- web/src/getDatasource.tsx
+++ web/src/getDatasource.tsx
@@ -11,20 +11,57 @@ const cache = new LRU<string, DatasourceInfo>({
   ttl: 1000 * 60 * 5,
 });
 
-async function getDataSource(datasourceName: string): Promise<DatasourceInfo> {
+// async function getDataSource(datasourceName: string): Promise<DatasourceInfo> {
+//   if (cache.has(datasourceName)) {
+//     return cache.get(datasourceName);
+//   }
+
+//   const datasource = await fetch(
+//     `${BACKEND_API}/api/v1/datasources/${datasourceName}`,
+//   );
+
+//   try {
+//     const jsonData = await datasource.json();
+
+//     const basePath = `/api/proxy/plugin/console-dashboards-plugin/backend/proxy/${datasourceName}`;
+//     const dataSourceType = jsonData?.spec?.plugin?.kind;
+
+//     const datasourceInfo = { basePath, dataSourceType };
+
+//     cache.set(datasourceName, datasourceInfo);
+
+//     return datasourceInfo;
+//   } catch (err) {
+//     console.error(err);
+//   }
+
+//   return null;
+// }
+
+// export default getDataSource;
+
+
+
+// TODO: clean up this is for testing 
+function getDataSource(datasourceName: string): DatasourceInfo {
   if (cache.has(datasourceName)) {
     return cache.get(datasourceName);
   }
 
-  const datasource = await fetch(
-    `${BACKEND_API}/api/v1/datasources/${datasourceName}`,
-  );
+  // const datasource =  fetch(
+  //   `${BACKEND_API}/api/v1/datasources/${datasourceName}`,
+  // );
 
   try {
-    const jsonData = await datasource.json();
+    // const jsonData =  datasource.json();
+    // const dataSourceType = jsonData?.spec?.plugin?.kind;
+
+    // expected JSON object is 
+    // {"kind":"Datasource","metadata":{"name":"cluster-prometheus-proxy"},"spec":{"plugin":{"kind":"prometheus","spec":{"direct_url":""}}}}
+    // need to spoof jsonData becuase it fetches localhost:9000 instead of https://console-openshift-console.apps.rhoms-4.13-073104.dev.openshiftappsvc.org/
 
     const basePath = `/api/proxy/plugin/console-dashboards-plugin/backend/proxy/${datasourceName}`;
-    const dataSourceType = jsonData?.spec?.plugin?.kind;
+    const dataSourceType = "prometheus";
 
     const datasourceInfo = { basePath, dataSourceType };
 
